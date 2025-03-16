f1328adee371ce480742e19247e4483b13d87951
Xojimurod <151165550+iOSDew@users.noreply.github.com>
2025-03-16 21:58:31 +0500

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Khojimurod Sultonov on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTokCloneTests/TikTokCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneTests.swift
+//  TikTokCloneTests
+//
+//  Created by Khojimurod Sultonov on 16/03/25.
+//
+
+import Testing
+@testable import TikTokClone
+
+struct TikTokCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
